b4bbbc68ec3da8fdf8cc6b4c8316869f0cd56d57
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-06-06 12:04:54 -0700

    gat all thoughts

---
+++ controllers/thought-controller.js
@@ -0,0 +1,21 @@
+// path to Thought and user models
+const { Thought } = require("../models/Thought");
+const { User } = require("../models/User");
+
+const thoughtController = {
+  // get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .populate({
+        path: "User",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
