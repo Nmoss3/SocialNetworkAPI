23a34e5e6f962f52c90ee467117e993c1c385690
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-06-06 12:08:38 -0700

    get one thought by ID

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -18,4 +18,19 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  //   get 1 thought by ID
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "user",
+        select: "__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
