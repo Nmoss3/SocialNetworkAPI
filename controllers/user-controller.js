e711cbac68e6de1c1f13d3c2475f76b149a50063
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-06-06 11:03:46 -0700

    get user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,5 +17,24 @@ const userController = {
   },
 
   // get all users by ID using thoughts
-  getUserById({ params }, res) {},
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  //   create User
+  createUser({ body }, res) {},
 };
