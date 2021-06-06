3a92c695e98fe2e0e242fa0befe778c92f50f259
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-06-06 10:58:05 -0700

    userController/get all users

---
+++ controllers/user-controller.js
@@ -0,0 +1,21 @@
+const { User } = require("../models/User");
+
+const userController = {
+  // gets all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // get all users by ID using thoughts
+  getUserById({ params }, res) {},
+};
